sk-code-01/mcp-clickhouse-server

Allow specifying bind host and bind port (#64)

mcp_clickhouse/mcp_env.py
@@ -7,6 +7,20 @@ and type conversion.
 from dataclasses import dataclass
 import os
 from typing import Optional
+from enum import Enum
+
+
+class TransportType(str, Enum):
+    """Supported MCP server transport types."""
+
+    STDIO = "stdio"
+    HTTP = "http"
+    SSE = "sse"
+
+    @classmethod
+    def values(cls) -> list[str]:
+        """Get all valid transport values."""
+        return [transport.value for transport in cls]
 
 
 @dataclass
@@ -30,6 +44,8 @@ class ClickHouseConfig:
         CLICKHOUSE_DATABASE: Default database to use (default: None)
         CLICKHOUSE_PROXY_PATH: Path to be added to the host URL. For instance, for servers behind an HTTP proxy (default: None)
         CLICKHOUSE_MCP_SERVER_TRANSPORT: MCP server transport method - "stdio", "http", or "sse" (default: stdio)
+        CLICKHOUSE_MCP_BIND_HOST: Host to bind the MCP server to when using HTTP or SSE transport (default: 127.0.0.1)
+        CLICKHOUSE_MCP_BIND_PORT: Port to bind the MCP server to when using HTTP or SSE transport (default: 8000)
         CLICKHOUSE_ENABLED: Enable ClickHouse server (default: true)
     """
 
@@ -120,13 +136,31 @@ class ClickHouseConfig:
         Valid options: "stdio", "http", "sse"
         Default: "stdio"
         """
-        transport = os.getenv("CLICKHOUSE_MCP_SERVER_TRANSPORT", "stdio").lower()
-        valid_transports = "stdio", "http", "streamable-http", "sse"
-        if transport not in valid_transports:
-            raise ValueError(
-                f"Invalid transport '{transport}'. Valid options: {', '.join(valid_transports)}"
-            )
+        transport = os.getenv("CLICKHOUSE_MCP_SERVER_TRANSPORT", TransportType.STDIO.value).lower()
+
+        # Validate transport type
+        if transport not in TransportType.values():
+            valid_options = ", ".join(f'"{t}"' for t in TransportType.values())
+            raise ValueError(f"Invalid transport '{transport}'. Valid options: {valid_options}")
         return transport
+
+    @property
+    def mcp_bind_host(self) -> str:
+        """Get the host to bind the MCP server to.
+
+        Only used when transport is "http" or "sse".
+        Default: "127.0.0.1"
+        """
+        return os.getenv("CLICKHOUSE_MCP_BIND_HOST", "127.0.0.1")
+
+    @property
+    def mcp_bind_port(self) -> int:
+        """Get the port to bind the MCP server to.
+
+        Only used when transport is "http" or "sse".
+        Default: 8000
+        """
+        return int(os.getenv("CLICKHOUSE_MCP_BIND_PORT", "8000"))
 
     def get_client_config(self) -> dict:
         """Get the configuration dictionary for clickhouse_connect client.
@@ -239,7 +273,7 @@ def get_chdb_config() -> ChDBConfig:
     """
     Gets the singleton instance of ChDBConfig.
     Instantiates it on the first call.
-    
+
     Returns:
         ChDBConfig: The chDB configuration instance
     """

mcp_clickhouse/mcp_server.py
@@ -12,7 +12,10 @@ from dotenv import load_dotenv
 from fastmcp import FastMCP
 from fastmcp.tools import Tool
 from fastmcp.prompts import Prompt
+from fastmcp.exceptions import ToolError
 from dataclasses import dataclass, field, asdict, is_dataclass
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
 
 from mcp_clickhouse.mcp_env import get_config, get_chdb_config
 from mcp_clickhouse.chdb_prompt import CHDB_PROMPT
@@ -73,6 +76,22 @@ mcp = FastMCP(
         "chdb",
     ],
 )
+
+
+@mcp.custom_route("/health", methods=["GET"])
+async def health_check(request: Request) -> PlainTextResponse:
+    """Health check endpoint for monitoring server status.
+
+    Returns OK if the server is running and can connect to ClickHouse.
+    """
+    try:
+        # Try to create a client connection to verify ClickHouse connectivity
+        client = create_clickhouse_client()
+        version = client.server_version
+        return PlainTextResponse(f"OK - Connected to ClickHouse {version}")
+    except Exception as e:
+        # Return 503 Service Unavailable if we can't connect to ClickHouse
+        return PlainTextResponse(f"ERROR - Cannot connect to ClickHouse: {str(e)}", status_code=503)
 
 
 def result_to_table(query_columns, result) -> List[Table]:
@@ -150,9 +169,7 @@ def execute_query(query: str):
         return {"columns": res.column_names, "rows": res.result_rows}
     except Exception as err:
         logger.error(f"Error executing query: {err}")
-        # Return a structured dictionary rather than a string to ensure proper serialization
-        # by the MCP protocol. String responses for errors can cause BrokenResourceError.
-        return {"error": str(err)}
+        raise ToolError(f"Query execution failed: {str(err)}")
 
 
 def run_select_query(query: str):
@@ -175,16 +192,12 @@ def run_select_query(query: str):
         except concurrent.futures.TimeoutError:
             logger.warning(f"Query timed out after {SELECT_QUERY_TIMEOUT_SECS} seconds: {query}")
             future.cancel()
-            # Return a properly structured response for timeout errors
-            return {
-                "status": "error",
-                "message": f"Query timed out after {SELECT_QUERY_TIMEOUT_SECS} seconds",
-            }
+            raise ToolError(f"Query timed out after {SELECT_QUERY_TIMEOUT_SECS} seconds")
+    except ToolError:
+        raise
     except Exception as e:
         logger.error(f"Unexpected error in run_select_query: {str(e)}")
-        # Catch all other exceptions and return them in a structured format
-        # to prevent MCP serialization failures
-        return {"status": "error", "message": f"Unexpected error: {str(e)}"}
+        raise RuntimeError(f"Unexpected error during query execution: {str(e)}")
 
 
 def create_clickhouse_client():
@@ -249,7 +262,7 @@ def execute_chdb_query(query: str):
     """Execute a query using chDB client."""
     client = create_chdb_client()
     try:
-        res = client.query(query, 'JSON')
+        res = client.query(query, "JSON")
         if res.has_error():
             error_msg = res.error_message()
             logger.error(f"Error executing chDB query: {error_msg}")
@@ -310,7 +323,7 @@ def _init_chdb_client():
             return None
 
         client_config = get_chdb_config().get_client_config()
-        data_path = client_config['data_path']
+        data_path = client_config["data_path"]
         logger.info(f"Creating chDB client with data_path={data_path}")
         client = chs.Session(path=data_path)
         logger.info(f"Successfully connected to chDB with data_path={data_path}")
@@ -337,7 +350,7 @@ if os.getenv("CHDB_ENABLED", "false").lower() == "true":
     chdb_prompt = Prompt.from_function(
         chdb_initial_prompt,
         name="chdb_initial_prompt",
-        description="This prompt helps users understand how to interact and perform common operations in chDB"
+        description="This prompt helps users understand how to interact and perform common operations in chDB",
     )
     mcp.add_prompt(chdb_prompt)
     logger.info("chDB tools and prompts registered")

hydrolix/mcp-hydrolix

Co-authored-by: Ethan Bell <ebell@hydrolix.io>

mcp_hydrolix/mcp_env.py
@@ -7,6 +7,20 @@ and type conversion.
 from dataclasses import dataclass
 import os
 from typing import Optional
+from enum import Enum
+
+
+class TransportType(str, Enum):
+    """Supported MCP server transport types."""
+
+    STDIO = "stdio"
+    HTTP = "http"
+    SSE = "sse"
+
+    @classmethod
+    def values(cls) -> list[str]:
+        """Get all valid transport values."""
+        return [transport.value for transport in cls]
 
 
 @dataclass
@@ -29,6 +43,8 @@ class HydrolixConfig:
         HYDROLIX_DATABASE: Default database to use (default: None)
         HYDROLIX_PROXY_PATH: Path to be added to the host URL. For instance, for servers behind an HTTP proxy (default: None)
         HYDROLIX_MCP_SERVER_TRANSPORT: MCP server transport method - "stdio", "http", or "sse" (default: stdio)
+        HYDROLIX_MCP_BIND_HOST: Host to bind the MCP server to when using HTTP or SSE transport (default: 127.0.0.1)
+        HYDROLIX_MCP_BIND_PORT: Port to bind the MCP server to when using HTTP or SSE transport (default: 8000)
     """
 
     def __init__(self):
@@ -89,6 +105,7 @@ class HydrolixConfig:
         Default: 300 (Hydrolix default)
         """
         return int(os.getenv("HYDROLIX_SEND_RECEIVE_TIMEOUT", "300"))
+
     @property
     def proxy_path(self) -> str:
         return os.getenv("HYDROLIX_PROXY_PATH")
@@ -100,13 +117,31 @@ class HydrolixConfig:
         Valid options: "stdio", "http", "sse"
         Default: "stdio"
         """
-        transport = os.getenv("HYDROLIX_MCP_SERVER_TRANSPORT", "stdio").lower()
-        valid_transports = "stdio", "http", "streamable-http", "sse"
-        if transport not in valid_transports:
-            raise ValueError(
-                f"Invalid transport '{transport}'. Valid options: {', '.join(valid_transports)}"
-            )
+        transport = os.getenv("HYDROLIX_MCP_SERVER_TRANSPORT", TransportType.STDIO.value).lower()
+
+        # Validate transport type
+        if transport not in TransportType.values():
+            valid_options = ", ".join(f'"{t}"' for t in TransportType.values())
+            raise ValueError(f"Invalid transport '{transport}'. Valid options: {valid_options}")
         return transport
+
+    @property
+    def mcp_bind_host(self) -> str:
+        """Get the host to bind the MCP server to.
+
+        Only used when transport is "http" or "sse".
+        Default: "127.0.0.1"
+        """
+        return os.getenv("HYDROLIX_MCP_BIND_HOST", "127.0.0.1")
+
+    @property
+    def mcp_bind_port(self) -> int:
+        """Get the port to bind the MCP server to.
+
+        Only used when transport is "http" or "sse".
+        Default: 8000
+        """
+        return int(os.getenv("HYDROLIX_MCP_BIND_PORT", "8000"))
 
     def get_client_config(self) -> dict:
         """Get the configuration dictionary for clickhouse_connect client.

mcp_hydrolix/mcp_server.py
@@ -8,7 +8,10 @@ import clickhouse_connect
 from clickhouse_connect.driver.binding import format_query_value
 from dotenv import load_dotenv
 from fastmcp import FastMCP
+from fastmcp.exceptions import ToolError
 from dataclasses import dataclass, field, asdict, is_dataclass
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse
 
 from mcp_hydrolix.mcp_env import get_config
 
@@ -67,6 +70,22 @@ mcp = FastMCP(
         "pip-system-certs",
     ],
 )
+
+
+@mcp.custom_route("/health", methods=["GET"])
+async def health_check(request: Request) -> PlainTextResponse:
+    """Health check endpoint for monitoring server status.
+
+    Returns OK if the server is running and can connect to Hydrolix.
+    """
+    try:
+        # Try to create a client connection to verify query-head connectivity
+        client = create_hydrolix_client()
+        version = client.server_version
+        return PlainTextResponse(f"OK - Connected to Hydrolix compatible with ClickHouse {version}")
+    except Exception as e:
+        # Return 503 Service Unavailable if we can't connect to Hydrolix
+        return PlainTextResponse(f"ERROR - Cannot connect to Hydrolix: {str(e)}", status_code=503)
 
 
 def result_to_table(query_columns, result) -> List[Table]:
@@ -155,9 +174,7 @@ def execute_query(query: str):
         return {"columns": res.column_names, "rows": res.result_rows}
     except Exception as err:
         logger.error(f"Error executing query: {err}")
-        # Return a structured dictionary rather than a string to ensure proper serialization
-        # by the MCP protocol. String responses for errors can cause BrokenResourceError.
-        return {"error": str(err)}
+        raise ToolError(f"Query execution failed: {str(err)}")
 
 
 @mcp.tool()
@@ -217,16 +234,12 @@ def run_select_query(query: str):
         except concurrent.futures.TimeoutError:
             logger.warning(f"Query timed out after {SELECT_QUERY_TIMEOUT_SECS} seconds: {query}")
             future.cancel()
-            # Return a properly structured response for timeout errors
-            return {
-                "status": "error",
-                "message": f"Query timed out after {SELECT_QUERY_TIMEOUT_SECS} seconds",
-            }
+            raise ToolError(f"Query timed out after {SELECT_QUERY_TIMEOUT_SECS} seconds")
+    except ToolError:
+        raise
     except Exception as e:
         logger.error(f"Unexpected error in run_select_query: {str(e)}")
-        # Catch all other exceptions and return them in a structured format
-        # to prevent MCP serialization failures
-        return {"status": "error", "message": f"Unexpected error: {str(e)}"}
+        raise RuntimeError(f"Unexpected error during query execution: {str(e)}")
 
 
 def create_hydrolix_client():
@@ -243,7 +256,7 @@ def create_hydrolix_client():
         client = clickhouse_connect.get_client(**client_config)
         # Test the connection
         version = client.server_version
-        logger.info(f"Successfully connected to Hydrolix server version {version}")
+        logger.info(f"Successfully connected to Hydrolix compatible with ClickHouse {version}")
         return client
     except Exception as e:
         logger.error(f"Failed to connect to Hydrolix: {str(e)}")