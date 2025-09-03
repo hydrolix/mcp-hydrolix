sk-code-01/mcp-clickhouse-server

Migrate to fastmcp - it's receiving more regular updates (#59)
1. Also >= python 3.10
2. Allow for configurable transports
3. Add comprehensive test suite using mcp client as well

mcp_clickhouse/mcp_env.py
@@ -29,6 +29,7 @@ class ClickHouseConfig:
         CLICKHOUSE_SEND_RECEIVE_TIMEOUT: Send/receive timeout in seconds (default: 300)
         CLICKHOUSE_DATABASE: Default database to use (default: None)
         CLICKHOUSE_PROXY_PATH: Path to be added to the host URL. For instance, for servers behind an HTTP proxy (default: None)
+        CLICKHOUSE_MCP_SERVER_TRANSPORT: MCP server transport method - "stdio", "http", or "sse" (default: stdio)
     """
 
     def __init__(self):
@@ -97,10 +98,25 @@ class ClickHouseConfig:
         Default: 300 (ClickHouse default)
         """
         return int(os.getenv("CLICKHOUSE_SEND_RECEIVE_TIMEOUT", "300"))
-    
+
     @property
     def proxy_path(self) -> str:
         return os.getenv("CLICKHOUSE_PROXY_PATH")
+
+    @property
+    def mcp_server_transport(self) -> str:
+        """Get the MCP server transport method.
+
+        Valid options: "stdio", "http", "sse"
+        Default: "stdio"
+        """
+        transport = os.getenv("CLICKHOUSE_MCP_SERVER_TRANSPORT", "stdio").lower()
+        valid_transports = "stdio", "http", "streamable-http", "sse"
+        if transport not in valid_transports:
+            raise ValueError(
+                f"Invalid transport '{transport}'. Valid options: {', '.join(valid_transports)}"
+            )
+        return transport
 
     def get_client_config(self) -> dict:
         """Get the configuration dictionary for clickhouse_connect client.
@@ -123,7 +139,7 @@ class ClickHouseConfig:
         # Add optional database if set
         if self.database:
             config["database"] = self.database
-        
+
         if self.proxy_path:
             config["proxy_path"] = self.proxy_path
 

hydrolix/mcp-hydrolix

(cherry picked from commit 52f1b98d3e42fa8119b703938ebced9aa8af72bb)

mcp_hydrolix/mcp_env.py
@@ -28,6 +28,7 @@ class HydrolixConfig:
         HYDROLIX_SEND_RECEIVE_TIMEOUT: Send/receive timeout in seconds (default: 300)
         HYDROLIX_DATABASE: Default database to use (default: None)
         HYDROLIX_PROXY_PATH: Path to be added to the host URL. For instance, for servers behind an HTTP proxy (default: None)
+        HYDROLIX_MCP_SERVER_TRANSPORT: MCP server transport method - "stdio", "http", or "sse" (default: stdio)
     """
 
     def __init__(self):
@@ -88,10 +89,24 @@ class HydrolixConfig:
         Default: 300 (Hydrolix default)
         """
         return int(os.getenv("HYDROLIX_SEND_RECEIVE_TIMEOUT", "300"))
-
     @property
     def proxy_path(self) -> str:
         return os.getenv("HYDROLIX_PROXY_PATH")
+
+    @property
+    def mcp_server_transport(self) -> str:
+        """Get the MCP server transport method.
+
+        Valid options: "stdio", "http", "sse"
+        Default: "stdio"
+        """
+        transport = os.getenv("HYDROLIX_MCP_SERVER_TRANSPORT", "stdio").lower()
+        valid_transports = "stdio", "http", "streamable-http", "sse"
+        if transport not in valid_transports:
+            raise ValueError(
+                f"Invalid transport '{transport}'. Valid options: {', '.join(valid_transports)}"
+            )
+        return transport
 
     def get_client_config(self) -> dict:
         """Get the configuration dictionary for clickhouse_connect client.